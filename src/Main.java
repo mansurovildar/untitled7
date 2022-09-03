653f12cb9f831342f2f48c8687719099a70e3370
dev <>
2022-09-03 18:31:22 +0300

    домашнее задание коммит7

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задание 3-1
+        int age = 17;
+        if (age>=18) {
+            System.out.println("Поздравляем с совершенолетием");
+        }
+        if (age<18) {
+            System.out.println("Возраст совершеннолетия ещё не наступил, и нужно немного подождать");
+        }
+        // Задание 3-2
+
+        if (age >= 7) {
+            System.out.println("Ребенок ходит в школу");
+        }
+        if (age >= 18){
+            System.out.println("Человек закончил школу и может поступать в университет");
+        }
+        if (age > 24) {
+            System.out.println("Пипец окончил университет и пора искать первую работу");
+        }
+
+
+
+
+
+    }
+}
